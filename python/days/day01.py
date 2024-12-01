f954f5d29843d3ceef60d01fce7974bbb0db9166
Mike Heidal <mikeheidal@gmail.com>
2024-11-30 21:06:51 -0800

    Day 1

--- python/days/day01.py
+++ python/days/day01.py
@@ -3,22 +3,36 @@ from utils import utils
 
 def part_1():
     with open("inputs/day01.txt", "r") as f:
-        return
-
-
-def part_1_alt():
-    with open("inputs/day01.txt", "r") as f:
-        return
+        total = 0
+        l = []
+        r = []
+        for line in f.readlines():
+            nums = utils.ints(line)
+            l.append(nums[0])
+            r.append(nums[1])
+        l.sort()
+        r.sort()
+        for a, b in zip(l, r):
+            total += abs(a - b)
+        return total
 
 
 def part_2():
     with open("inputs/day01.txt", "r") as f:
-        return
-
-
-def part_2_alt():
-    with open("inputs/day01.txt", "r") as f:
-        return
+        total = 0
+        l = []
+        r = []
+        for line in f.readlines():
+            nums = utils.ints(line)
+            l.append(nums[0])
+            r.append(nums[1])
+        for a in l:
+            mul = 0
+            for b in r:
+                if a == b:
+                    mul += 1
+            total += a * mul
+        return total
 
 
 if __name__ == "__main__":
