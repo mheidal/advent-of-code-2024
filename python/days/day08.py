420b020b63e19fe0cea482c2a3cc9ab4e0f44915
Mike Heidal <mikeheidal@gmail.com>
2024-12-07 21:42:13 -0800

    Day 8

--- python/days/day08.py
+++ python/days/day08.py
@@ -1,29 +1,60 @@
+from collections import defaultdict
+import itertools
+from numpy import angle
 from utils import utils
 
 
 def part_1():
     with open("inputs/day08.txt", "r") as f:
-        return
-
-
-def part_1_alt():
-    with open("inputs/day08.txt", "r") as f:
-        return
+        frequencies = defaultdict(lambda: set())
+        width = height = 0
+        antinodes = set()
+        for i, row in enumerate(f.readlines()):
+            height = max(height, i)
+            for j, col in enumerate(row.strip()):
+                width = max(width, j)
+                if col != ".":
+                    frequencies[col].add(utils.com.from_row_col(i, j))
+        for row in range(height+1): #+1?
+            for col in range(width+1):
+                point = utils.com.from_row_col(row, col)
+                for _, antennae in frequencies.items():
+                    for a, b in itertools.combinations(antennae, 2):
+                        if a == point or b == point: continue
+                        a_point = a - point
+                        b_point = b - point
+                        if a_point == 2 * b_point or a_point == .5 * b_point:
+                            antinodes.add(point)
+        return len(antinodes)
 
 
 def part_2():
     with open("inputs/day08.txt", "r") as f:
-        return
-
-
-def part_2_alt():
-    with open("inputs/day08.txt", "r") as f:
-        return
+        frequencies = defaultdict(lambda: set())
+        width = height = 0
+        antinodes = set()
+        for i, row in enumerate(f.readlines()):
+            height = max(height, i)
+            for j, col in enumerate(row.strip()):
+                width = max(width, j)
+                if col != ".":
+                    frequencies[col].add(utils.com.from_row_col(i, j))
+        for row in range(height+1): #+1?
+            for col in range(width+1):
+                point = utils.com.from_row_col(row, col)
+                for _, antennae in frequencies.items():
+                    for a, b in itertools.combinations(antennae, 2):
+                        if a == point or b == point: 
+                            antinodes.add(point)
+                            continue
+                        a_point = a - point
+                        b_point = b - point
+                        if angle(a_point, deg=True) % 180 == angle(b_point, deg=True) % 180:
+                            antinodes.add(point)
+        return len(antinodes)
 
 
 if __name__ == "__main__":
     print(f"Part 1: {part_1()}")
-    print(f"Part 1 (alternate): {part_1_alt()}")
     print(f"Part 2: {part_2()}")
-    print(f"Part 2 (alternate): {part_2_alt()}")
 
