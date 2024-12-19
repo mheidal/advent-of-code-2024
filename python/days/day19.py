271aafcf48e511ab5e3607024830f9b1dc3e4928
Mike Heidal <mikeheidal@gmail.com>
2024-12-18 21:24:16 -0800

    Day 19

--- python/days/day19.py
+++ python/days/day19.py
@@ -1,26 +1,49 @@
-from utils import utils
+from functools import lru_cache
 
 
 def part_1():
     with open("inputs/day19.txt", "r") as f:
         text = f.read()
-    return
+    towels, designs = text.split("\n\n")
+    towels = towels.split(", ")
+    designs = designs.splitlines()
 
+    def recursively_build_design(design: str, start: int = 0):
+        if start == len(design):
+            return True
+        for towel in towels:
+            if len(towel) + start <= len(design):
+                if towel == design[start:start + len(towel)]:
+                    if recursively_build_design(design, start + len(towel)):
+                        return True
+        return False
 
-def part_1_alt():
-    with open("inputs/day19.txt", "r") as f:
-        text = f.read()
-    return
+    total = 0
+    for design in designs:
+        if recursively_build_design(design):
+            total += 1
+    return total
 
 
 def part_2():
     with open("inputs/day19.txt", "r") as f:
         text = f.read()
-    return
+    towels, designs = text.split("\n\n")
+    towels = towels.split(", ")
+    designs = designs.splitlines()
 
+    @lru_cache(maxsize=None)
+    def recursively_build_design(design: str, start: int = 0):
+        if start == len(design):
+            return 1
+        subtotal = 0
+        for towel in towels:
+            if len(towel) + start <= len(design):
+                if towel == design[start:start + len(towel)]:
+                    subtotal += recursively_build_design(design, start + len(towel))
+        return subtotal
 
-def part_2_alt():
-    with open("inputs/day19.txt", "r") as f:
-        text = f.read()
-    return
-
+    total = 0
+    for design in designs:
+        total += recursively_build_design(design)
+    return total
