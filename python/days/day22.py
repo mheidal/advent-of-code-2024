6537f4c27dc4dc2cd0606eb1f712f98c3c8dec23
Mike Heidal <mikeheidal@gmail.com>
2024-12-22 20:59:15 -0800

    Day 22 tweaks

--- python/days/day22.py
+++ python/days/day22.py
@@ -2,21 +2,15 @@ from collections import Counter
 from utils import utils
 
 
-def mix(a, s):
-    return a ^ s
-
-def prune(s):
-    return s % 16777216
-
 def part_1():
     with open("inputs/day22.txt", "r") as f:
         text = f.read()
     total = 0
     for num in utils.ints(text):
         for _ in range(2000):
-            num = prune(mix(num, num * 64))
-            num = prune(mix(num, num // 32))
-            num = prune(mix(num, num * 2048))
+            num = (num ^ (num << 6)) & 0xffffff
+            num = (num ^ (num >> 5))
+            num = (num ^ (num << 11)) & 0xffffff
         total += num
     return total
 
@@ -29,9 +23,9 @@ def part_2():
     for num in utils.ints(text):
         prices = [num % 10]
         for _ in range(2000):
-            num = prune(mix(num, num * 64))
-            num = prune(mix(num, num // 32))
-            num = prune(mix(num, num * 2048))
+            num = num ^ (num << 6) & 0xffffff
+            num = num ^ (num >> 5)
+            num = num ^ (num << 11) & 0xffffff
             prices.append(num % 10)
         price_diffs = [prices[i] - prices[i-1] for i in range(1, len(prices))]
         seen_price_diffs = set()
