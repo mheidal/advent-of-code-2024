837cb1ea5c92875ad535cf9b3450b8be3c13bc34
Mike Heidal <mikeheidal@gmail.com>
2024-12-24 21:34:58 -0800

    Day 25. Merry Christmas

--- python/days/day25.py
+++ python/days/day25.py
@@ -1,26 +1,21 @@
-from utils import utils
-
-
 def part_1():
     with open("inputs/day25.txt", "r") as f:
         text = f.read()
-    return
-
-
-def part_1_alt():
-    with open("inputs/day25.txt", "r") as f:
-        text = f.read()
-    return
-
-
-def part_2():
-    with open("inputs/day25.txt", "r") as f:
-        text = f.read()
-    return
-
-
-def part_2_alt():
-    with open("inputs/day25.txt", "r") as f:
-        text = f.read()
-    return
-
+    locks = []
+    keys = []
+    for item in text.split("\n\n"):
+        heights = [0] * 5
+        for row in item.splitlines():
+            for j, col in enumerate(row):
+                if col == "#":
+                    heights[j] += 1
+        if "." not in item.splitlines()[0]:
+            locks.append(heights)
+        else:
+            keys.append(heights)
+    total = 0
+    for lock in locks:
+        for key in keys:
+            if all(lock[i] + key[i] <= 7 for i in range(5)):
+                total += 1
+    return total
